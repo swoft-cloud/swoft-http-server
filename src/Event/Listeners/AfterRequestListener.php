a23023b1cca955d8a39104ec16ae644397aa3299
lilin <794774870@qq.com>
2018-02-28 01:00:48 +0800

    add event

--- src/Event/Listeners/AfterRequestListener.php
+++ src/Event/Listeners/AfterRequestListener.php
@@ -13,11 +13,6 @@ use Swoft\Http\Server\Event\HttpServerEvent;
  * 请求后事件
  *
  * @Listener(HttpServerEvent::AFTER_REQUEST)
- * @uses      AfterReceiveListener
- * @version   2017年08月30日
- * @author    stelin <phpcrazy@126.com>
- * @copyright Copyright 2010-2016 Swoft software
- * @license   PHP Version 7.x {@link http://www.php.net/license/3_0.txt}
  */
 class AfterRequestListener implements EventHandlerInterface
 {
